d595f7872f7236f4420dce93080d6ae34c6bcafd
Chris Hatch <starforgedcode@gmail.com>
2020-08-07 10:36:03 -0500

    Components part 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap'
+import React, {useState} from 'react'
+import logo from './logo.svg'
+import {Navbar, NavbarBrand} from 'reactstrap'
 import Menu from './components/MenuComponent'
-import './App.css';
+import './App.css'
+import {DISHES} from './shared/dishes'
 
 function App() {
-  return (
-    <div className="App">
-        <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-        </Navbar>
-        <Menu />
-    </div>
-  );
+    const [dishes, setDishes] = useState(DISHES)
+    return (
+        <div className="App">
+            <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu dishes={dishes} />
+        </div>
+    )
 }
 
-export default App;
+export default App
