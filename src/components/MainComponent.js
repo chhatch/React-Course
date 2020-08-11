69cc04a478838de00e248f8dca83813d74bc75b2
Chris Hatch <starforgedcode@gmail.com>
2020-08-11 11:28:45 -0500

    Header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
-import {Navbar, NavbarBrand} from 'reactstrap'
 import Menu from './MenuComponent'
 import DishDetail from './DishDetailComponent'
+import Header from './HeaderComponent'
+import Footer from './FooterComponent'
 import {DISHES} from '../shared/dishes'
 
 function Main() {
@@ -9,13 +10,10 @@ function Main() {
     const [selectedDishId, setSelectedDishId] = useState(null)
     return (
         <div className="App">
-            <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                </div>
-            </Navbar>
+            <Header />
             <Menu dishes={dishes}  onClick={dishId => setSelectedDishId(dishId)} />
             <DishDetail dish={dishes.find(dish => dish.id === selectedDishId)} />
+            <Footer />
         </div>
     )
 }
